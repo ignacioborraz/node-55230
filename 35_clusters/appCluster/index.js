5377d008f11d808f86516d3a51823ee85599a574
ignacio.borraz <ignacioborraz@hotmail.com>
2023-10-10 20:58:06 -0300

    35

--- 35_clusters/appCluster/index.js
+++ 35_clusters/appCluster/index.js
@@ -2,12 +2,17 @@ import "dotenv/config.js";
 
 import express from "express";
 import { connect } from "mongoose";
+import cluster from "cluster";
+import { cpus } from "os";
 
+const numberOfProcess = cpus().length;
+//console.log({ numberOfProcess });
 const port = process.env.PORT || 8090;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 //operacion simple
 app.get("/simple", (req, res) => {
   let counter = 0;
@@ -22,12 +27,23 @@ app.get("/complex", (req, res) => {
   for (let i = 1; i <= 1000000000; i++) {
     counter = counter + i;
   }
+  console.log(process.pid);
   return res.status(200).json({ counter });
 });
 
-app.listen(port, () => {
-  console.log("server ready on port " + port);
-  connect(process.env.LINK_DB)
-    .then(() => console.log("connected to db"))
-    .catch((err) => console.log(err));
-});
+//crear workers/nodos
+//console.log(cluster.isPrimary);
+if (cluster.isPrimary) {
+  console.log('primary');
+  for (let i=1; i<=numberOfProcess; i++) {
+    cluster.fork()
+  }
+} else {
+  console.log('worker',process.pid);
+  app.listen(port, () => {
+    console.log("server ready on port " + port);
+    connect(process.env.LINK_DB)
+      .then(() => console.log("connected to db"))
+      .catch((err) => console.log(err));
+  });
+}
--- 35_clusters/appDocker/index.js
+++ 35_clusters/appDocker/index.js
@@ -1,14 +1,18 @@
 import "dotenv/config.js";
 
 import express from "express";
+import morgan from "morgan";
 import { connect } from "mongoose";
 
 const port = process.env.PORT || 8090;
 const app = express();
 
+//middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//operacion simple
+app.use(morgan("dev"));
+
+//endpoints
 app.get("/simple", (req, res) => {
   let counter = 0;
   for (let i = 1; i <= 100; i++) {
@@ -16,7 +20,6 @@ app.get("/simple", (req, res) => {
   }
   return res.status(200).json({ counter });
 });
-//operacion compleja
 app.get("/complex", (req, res) => {
   let counter = 0;
   for (let i = 1; i <= 1000000000; i++) {
@@ -25,6 +28,7 @@ app.get("/complex", (req, res) => {
   return res.status(200).json({ counter });
 });
 
+//server & connection
 app.listen(port, () => {
   console.log("server ready on port " + port);
   connect(process.env.LINK_DB)
