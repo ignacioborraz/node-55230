1f10ceff3520f8209565701a73f4990c1004fead
ignacio.borraz <ignacioborraz@hotmail.com>
2023-10-17 20:00:21 -0300

    router

---
+++ 36_integracion/src/middlewares/errorHandler.js
@@ -0,0 +1,6 @@
+export default function(error,req,res,next) {
+    return res.status(500).json({
+        message: error.message,
+        response: error.fileName + ": " + error.lineNumber
+    })
+}
