1277a6a2edca722ffa51e82183e6d50c38244fd7
ignacio.borraz <ignacioborraz@hotmail.com>
2023-10-25 09:58:04 -0300

    38

--- 38_seguridad/proyecto1/src/app.js
+++ 38_seguridad/proyecto1/src/app.js
@@ -3,6 +3,7 @@ import handlebars from 'express-handlebars';
 import __dirname from './utils.js';
 import viewsRouter from './routes/views.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+//deberiamos llamar al enrutador principal
 
 const app = express();
 
@@ -15,3 +16,4 @@ app.use(express.urlencoded({extended:true}))
 app.use('/',viewsRouter);
 app.use('/api/sessions',sessionsRouter);
 app.listen(8080,()=>console.log(`Listening on PORT 8080`))
+//TODO lo que se hardcodea es considerado una vulnerabilidad
--- 38_seguridad/proyecto1/src/routes/views.router.js
+++ 38_seguridad/proyecto1/src/routes/views.router.js
@@ -5,5 +5,6 @@ const router = Router();
 router.get('/',(req,res)=>{
     res.render('register');
 })
+//deberia llamar al controlador
 
 export default router;
